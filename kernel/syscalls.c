648b8862456bf85f649cecd5700743fce64fc1c1
Corentin Hautefaye <the.r3fraction@gmail.com>
2025-01-12 22:15:03 +0100

    Syscalls fix

--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "tty.h"
+#include "io.h"
 
 void do_syscalls(u32 id) {
   char* str;
@@ -9,11 +10,14 @@ void do_syscalls(u32 id) {
     case 1: { // Display text
       asm("mov %%ebx, %0": "=m"(str): );
 	    for (i = 0; i < 100000; i++);
+      cli;
       terminal_write(str);
-
+      sti;
 	    break;
 	  }
 	  default:
 	    terminal_write("Unknown system call!\n");
   }
+
+  return;
 }
